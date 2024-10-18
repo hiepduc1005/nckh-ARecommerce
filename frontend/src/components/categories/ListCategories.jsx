a5ee857864009b10cbe9b315df85a68d04b0ea14
duc hiep <hiep100524@gmail.com>
2024-10-18 10:19:28 +0700

    update

--- frontend/src/components/categories/ListCategories.jsx
+++ frontend/src/components/categories/ListCategories.jsx
@@ -1,8 +1,79 @@
 import React from 'react'
-
+import './ListCategories.css'
+import categoriesImage from '../../assets/image/thoi-trang-nam.png'
 const ListCategories = () => {
   return (
-    <div>ListCategories</div>
+    <div className='list-categories'>
+      <div className="content">DANH MỤC NGÀNH HÀNG</div>
+      <div className="categories">
+        <div className="category-item">
+          <div className="category-image">
+            <img src={categoriesImage}></img>
+          </div>
+          <div className="category-name">Thời trang nam</div>
+        </div>
+        <div className="category-item">
+          <div className="category-image">
+            <img src={categoriesImage}></img>
+          </div>
+          <div className="category-name">Thời trang nam</div>
+        </div>
+        <div className="category-item">
+          <div className="category-image">
+            <img src={categoriesImage}></img>
+          </div>
+          <div className="category-name">Thời trang nam</div>
+        </div>
+        <div className="category-item">
+          <div className="category-image">
+            <img src={categoriesImage}></img>
+          </div>
+          <div className="category-name">Thời trang nam</div>
+        </div>
+        <div className="category-item">
+          <div className="category-image">
+            <img src={categoriesImage}></img>
+          </div>
+          <div className="category-name">Thời trang nam</div>
+        </div>
+        <div className="category-item">
+          <div className="category-image">
+            <img src={categoriesImage}></img>
+          </div>
+          <div className="category-name">Thời trang nam</div>
+        </div>
+        <div className="category-item">
+          <div className="category-image">
+            <img src={categoriesImage}></img>
+          </div>
+          <div className="category-name">Thời trang nam</div>
+        </div>
+        <div className="category-item">
+          <div className="category-image">
+            <img src={categoriesImage}></img>
+          </div>
+          <div className="category-name">Thời trang nam</div>
+        </div>
+        <div className="category-item">
+          <div className="category-image">
+            <img src={categoriesImage}></img>
+          </div>
+          <div className="category-name">Thời trang nam</div>
+        </div>
+        <div className="category-item">
+          <div className="category-image">
+            <img src={categoriesImage}></img>
+          </div>
+          <div className="category-name">Thời trang nam</div>
+        </div>
+        <div className="category-item">
+          <div className="category-image">
+            <img src={categoriesImage}></img>
+          </div>
+          <div className="category-name">Thời trang nam</div>
+        </div>
+      </div>
+    </div>
   )
 }
 
