fb70d4ee3a5ad77562c341e6668a8b3d42a69a1c
hiepduc1005 <hiepngo1524@gmail.com>
2025-03-14 20:42:33 +0700

    update userAddressApi

---
+++ frontend/src/api/userAddressApi.jsx
@@ -0,0 +1,87 @@
+import { toast } from "react-toastify";
+import axiosInstance from "../utils/axiosInstance";
+
+export const userAddressById = async (userAddressId,token) => {
+    try {
+        const res = await axiosInstance.get(`api/v1/user-address/${userAddressId}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }
+        )
+
+        return res.data;
+    } catch (error) {
+        console.error()
+        return null;
+    }
+}
+
+export const createUserAddress = async (data,token) => {
+    try {
+        const res = await axiosInstance.post("api/v1/user-address", data , 
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }
+        )
+
+        if(res.data == 200){
+            toast.success("Tạo địa chỉ thành công!")
+        }else {
+            toast.error("Tạo chỉ thất bại!")
+        }
+
+        return res.data
+        
+    } catch (error) {
+        console.error("Login failed",error)
+        toast.error("Tạo chỉ thất bại!")
+        return null;
+    }
+}
+
+
+
+export const updateUserAddress = async (data,token) => {
+    try {
+        const res = await axiosInstance.put("api/v1/user-address", data , {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+
+        if(res.data == 200){
+            toast.success("Cập nhập địa chỉ thành công!")
+        }else {
+            toast.error("Cập nhập chỉ thất bại!")
+        }
+        return res.data;
+        
+    } catch (error) {
+        toast.error("Cập nhập chỉ thất bại!")
+        return null;
+    }
+}
+
+export const deleteUserAddress = async (userAddressId,token) => {
+    try {
+        const res = await axiosInstance.delete(`api/v1/user-address/${userAddressId}`, data , {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+
+        if(res.status == 200){
+            toast.success("Xóa địa chỉ thành công!")
+        }else{
+            toast.error("Xóa địa chỉ thất bại!")
+        }
+        
+    } catch (error) {
+        
+        toast.error("Xóa địa chỉ thất bại!")
+    }
+}
